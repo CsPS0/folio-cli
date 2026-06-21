3a1caa4ceb2750f6b606def23e6e17f7a59bd7d8
Solti Csongor Péter <solti.csongor.peter@gmail.com>
2026-06-21 16:54:21 +0200

    chore: update scoop and homebrew configurations for v1.2.0

--- Formula/folio-cli.rb
+++ Formula/folio-cli.rb
@@ -1,16 +1,16 @@
 class FolioCli < Formula
   desc "Terminal-based client for the Hungarian Kréta electronic school diary system"
   homepage "https://github.com/CsPS0/folio-cli"
-  version "1.0.3"
+  version "1.2.0"
   license "MIT"
 
   on_macos do
-    url "https://github.com/CsPS0/folio-cli/releases/download/v1.0.3/folio-cli-macos"
+    url "https://github.com/CsPS0/folio-cli/releases/download/v1.2.0/folio-cli-macos"
     sha256 "de68ed50ee753824771e77780733e22e8ebe7774eccab5bdc0076b3c00de4c5b"
   end
 
   on_linux do
-    url "https://github.com/CsPS0/folio-cli/releases/download/v1.0.3/folio-cli-linux"
+    url "https://github.com/CsPS0/folio-cli/releases/download/v1.2.0/folio-cli-linux"
     sha256 "a2fa26b22c19735fddbc802221b04f62cd462443df381be2df650000c213aadf"
   end
 
